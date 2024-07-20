483a24ada6d78858d067e33103c532073c423332
mhasaero <eroouji7314@gmail.com>
2024-07-20 20:23:34 +0700

    feat(home): fix image bounty

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <main className=" font-sans">
-      <div className="absolute top-0 w-full h-screen md:h-[45rem] xl:h-screen bg-[url('/img/bounty-board.png')] bg-cover bg-fixed bg-center"></div>
+      <div className="absolute top-0 w-full h-[45rem] xl:h-screen bg-[url('/img/bounty-board.png')] bg-cover bg-fixed bg-center"></div>
       <Home />
       <About />
       <Skills />
